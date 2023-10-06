e5ecc88aab07b091b7e34d1492f830b5b0ed7a71
bhargavpoosa <101353589+bhargavpoosa@users.noreply.github.com>
2023-10-06 12:14:21 +0530

    Update login.js

--- PX-Html/loginpage/login.js
+++ PX-Html/loginpage/login.js
@@ -4,6 +4,11 @@ loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
+    let fatherName;
+    if(username==="sbpoosa")
+        fatherName = "xyz";
+    else
+        fatherName= "abc";
     if ((username === "sbpoosa" && password === "1234") || (username === "Sasuke" && password === "1234")) 
     {
         var id = username.substr(0,5);
@@ -18,7 +23,9 @@ loginButton.addEventListener("click", (e) => {
             "signUpDate": 1522697426479, //unix time in ms
             "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
             "price" : 95.5,
-            "userHash": "" // optional transient for HMAC identification
+            "userHash": "",
+            "fatherName": fatherName,  
+            // optional transient for HMAC identification
           },
           {
           //Account Fields
